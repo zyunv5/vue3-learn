10e44b1193e99d71ad368e5e00b6d01ec067d2fe
张允 <zyunv513@gmail.com>
2020-10-09 18:09:26 +0800

    vue3 init

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <form action="">
+    <input type="text" >
+  </form>
+  <ul>
+    <li v-for="(stu,index) in stus" :key="stu.id" @Click="removeItem(index)">
+      {{stu.name}}--{{stu.age}}
+    </li>
+  </ul>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  data(){
+    return{
+      stu:{id:"",name:"",age:""},
+      stus:[
+        {id:1,name:'aa',age:10},
+        {id:2,name:'bb',age:20},
+        {id:3,name:'cc',age:30},
+      ]
+    }
+  },
+  components: {
+    HelloWorld,
+  },
+  methods: {
+    removeItem(index){
+      console.log(index)
+      this.stus.splice(index,1)
+    }
+  },
+};
+</script>
---
+++ src/App1.vue
@@ -0,0 +1,15 @@
+<template>
+
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  name: "App",
+  setup() {
+    //定义了一个名称叫做count变量，这个变量的初始值是0
+    // 这个变量发生改变后，Vue会自动更新UI
+    let count = ref(0);
+  },
+};
+</script>
